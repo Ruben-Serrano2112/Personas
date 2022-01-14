d86e52624f5b205e85c6987d9190609a696ba6bc
Ruben <ruben.serrano@iesdoctorbalmis.com>
2022-01-14 12:54:36 +0100

    Agregar archivos de proyecto.

---
+++ Personas/AbrirVistaServicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    class AbrirVistaServicio
+    {
+        public void AbrirVentanaNuevaPersona()
+        {
+            NuevaPersona nueva = new NuevaPersona();
+            nueva.Show();
+        }
+    }
+}
---
+++ Personas/MainWindowVM.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personas
+{
+    class MainWindowVM : ObservableObject
+    {
+        public RelayCommand AbrirVentanaCommand { get; }
+        public AbrirVistaServicio AbrirServicio { get; }
+        public MainWindowVM()
+        {
+            AbrirVentanaCommand = new RelayCommand(AbrirVentana);
+            AbrirServicio = new AbrirVistaServicio();
+        }
+        private void AbrirVentana()
+        {
+            AbrirServicio.AbrirVentanaNuevaPersona();
+        }
+    }
+}
